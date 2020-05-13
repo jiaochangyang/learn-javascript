174aaec03bb9711b1281d9f4f33e11ee12815a4e
Chang Yang Jiao <cyjiao285@gmail.com>
2020-05-13 12:04:16 -0400

    Finished Mastering React: Advanced Topics > Context

--- learn-react/mastering-react/react-advanced/src/App.js
+++ learn-react/mastering-react/react-advanced/src/App.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import Movie from "./hoc/movie";
 import Counter from "./hooks/counter";
 import Users from "./hooks/users";
+import MoviePage from "./context/moviePage";
+import UserContext from "./context/userContext";
+import CartContext from "./context/cartContext";
+import Login from "./context/login";
 import "./App.css";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Movie id="1" />
-      <Counter />
-      <Users />
-    </React.Fragment>
-  );
+class App extends Component {
+  handleLoggedIn = (username) => {
+    console.log("Getting the user:", username);
+    const user = { name: "CYJ" };
+    this.setState({ currentUser: user });
+  };
+
+  state = { currentUser: { name: null } };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Movie id="1" />
+        <Counter />
+        <Users />
+        <CartContext.Provider value={{ cart: [] }}>
+          <UserContext.Provider
+            value={{
+              currentUser: this.state.currentUser,
+              onLoggedIn: this.handleLoggedIn,
+            }}
+          >
+            <MoviePage />
+            <Login />
+          </UserContext.Provider>
+        </CartContext.Provider>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
